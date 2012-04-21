1ff3b755ed1beceb5d2451571f563d8ccbba0881
Sergio Morales <sergiome@gmail.com>
2012-04-21 11:01:32 -0600

    Towns now appear, basic collisions are implemented, and added an animation for Mac.

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 
-WIDTH = 400
-HEIGHT = 250
+WIDTH = 300
+HEIGHT = 200
 SCALE = 3
 
 bgcolor = {240,240,255,255}
@@ -22,14 +22,20 @@ use_music = true
 
 gamestate = 1
 
---Direction of our main character
-
---0: right
---1: left
-
-direction = 1
 
 --load assets and initialize values
+
+
+macFrames = {}
+macFrames[0] = love.graphics.newQuad(0,0,78,106,719,106)
+macFrames[1] = love.graphics.newQuad(81,0,78,106,719,106)
+macFrames[2] = love.graphics.newQuad(161,0,78,106,719,106)
+macFrames[3] = love.graphics.newQuad(241,0,78,106,719,106)
+macFrames[4] = love.graphics.newQuad(321,0,78,106,719,106)
+macFrames[5] = love.graphics.newQuad(401,0,78,106,719,106)
+macFrames[6] = love.graphics.newQuad(481,0,78,106,719,106)
+macFrames[7] = love.graphics.newQuad(561,0,78,106,719,106)
+macFrames[8] = love.graphics.newQuad(641,0,78,106,719,106)
 
 function love.load()
 
@@ -52,7 +58,7 @@ function updateScale()
 	screenmiddlewidth = WIDTH*SCALE/2
 	screenmiddleheight = HEIGHT*SCALE/2
 	
-	macy=screenmiddleheight+65
+	macy=screenmiddleheight+12
 end
 
 function loadResources()
@@ -69,6 +75,12 @@ function loadResources()
 	
 	imgMacRight = love.graphics.newImage("gfx/macr.png")
 	imgMacRight:setFilter("nearest","nearest")
+	
+	imgMacRF = love.graphics.newImage("gfx/macrframes.png")
+	imgMacRF:setFilter("nearest","nearest")
+	
+	imgMacLF = love.graphics.newImage("gfx/maclframes.png")
+	imgMacLF:setFilter("nearest","nearest")
 
 	imgForeground = love.graphics.newImage("gfx/foreground.png")
 	imgForeground:setFilter("nearest","nearest")
@@ -114,12 +126,30 @@ end
 
 function restart()
 
+	--Direction of our main character
+
+	--0: right
+	--1: left
+
+	direction = 1
 	score = 0
 	worldSize = 1
 	rotation = 0
+	skyRotation = 0
 	jumping = false
+	goingDown = false
+	attacking = false
+	moving = false
+	canquit = false
 	yspeed = 1
-
+	currentMacFrame = 0
+	
+	
+	--City handling
+	cities = {}
+	citiesLife = {}
+	cityCount = 0
+	cityTimer = 2.5
 end
 
 
@@ -131,24 +161,60 @@ end
 
 function love.draw()
 
-	--Draw world
+	yRef = screenmiddleheight+65+imgBackground:getHeight()/2*worldSize*0.629
+	
+	--Draw the sky
+	love.graphics.draw( imgSky, screenmiddlewidth, screenmiddleheight+272, math.rad(skyRotation*360), 1, 1, imgSky:getWidth()/2, imgSky:getHeight()/2 )
+		
+	--Draw the background
+	love.graphics.draw( imgBackground, screenmiddlewidth, yRef, math.rad(rotation*360), worldSize, worldSize, imgBackground:getWidth()/2, imgBackground:getHeight()/2 )
 	
 	
-	--love.graphics.draw( imgSky, screenmiddlewidth, screenmiddleheight+600-((1.0-worldSize)*700), -rotation*0.25*6.28, 0.4, 0.4, imgSky:getWidth()/2, imgSky:getHeight()/2 )
+	--TODO: Draw cities
+	for i,city in ipairs(cities) do
+		love.graphics.draw( imgCity, screenmiddlewidth, yRef, math.rad(rotation*360)+city, 1, 1, imgCity:getWidth()/2, imgCity:getHeight() + yRef-screenmiddleheight-70)
+	end
 	
-	love.graphics.draw( imgBackground, screenmiddlewidth, screenmiddleheight+65+imgBackground:getHeight()/2*worldSize*0.629, rotation*6.28, worldSize, worldSize, imgBackground:getWidth()/2, imgBackground:getHeight()/2 )
+	--TODO: Draw citizens
+	
+	
+	love.graphics.print( macy , 400, 500)
+	
+	
+	if goingDown then
+		love.graphics.print( "going down!" , 400, 100)
+	end
+	
+	if attacking then
+		love.graphics.print( "attacking!" , 400, 150)
+	end
+	
+	if jumping then
+		love.graphics.print( "jumping!" , 400, 200)
+	end
+	
 	
 	--Draw mac!
 	if direction == 1 then
-		love.graphics.draw( imgMacLeft, screenmiddlewidth, macy, 0, 1, 1, imgMacLeft:getWidth()/2, imgMacLeft:getHeight())
+		frameToDraw = 0
+		-- Determine if moving, if true, animate
+		if moving == true then
+			frameToDraw = currentMacFrame
+		end
+		love.graphics.drawq( imgMacLF, macFrames[math.floor(frameToDraw)], screenmiddlewidth, macy, 0, 1, 1, 39, 53)
 	else
-		love.graphics.draw( imgMacRight, screenmiddlewidth, macy, 0, 1, 1, imgMacLeft:getWidth()/2, imgMacLeft:getHeight())
+		frameToDraw = 0
+		-- Determine if moving, if true, animate
+		if moving == true then
+			frameToDraw = currentMacFrame
+		end
+		love.graphics.drawq( imgMacRF, macFrames[math.floor(frameToDraw)], screenmiddlewidth, macy, 0, 1, 1, 39, 53)
 	end
 	
-	love.graphics.draw( imgForeground, screenmiddlewidth, screenmiddleheight+65+imgBackground:getHeight()/2*worldSize*0.629, rotation*6.28, worldSize, worldSize, imgBackground:getWidth()/2, imgBackground:getHeight()/2 )
+	--Draw the foreground
+	love.graphics.draw( imgForeground, screenmiddlewidth, yRef, math.rad(rotation*360), worldSize, worldSize, imgBackground:getWidth()/2, imgBackground:getHeight()/2 )
 	
-	--love.graphics.draw( imgForeground, screenmiddlewidth, screenmiddleheight+600-((1.0-worldSize)*700), rotation*6.28, worldSize, worldSize, imgBackground:getWidth()/2, imgBackground:getHeight()/2 )
-	
+	--Draw the pause screen
 	if gamestate == 2 then
 		love.graphics.draw( imgPauseScreen, 0, 0 )
 	end
@@ -171,8 +237,8 @@ function love.keypressed(key,unicode)
 		end	
 	
 
-	--IN-GAME KEYBOARD HANDLING
-    elseif gamestate == 1 then
+	--IN-GAME/ENDGAME KEYBOARD HANDLING
+    elseif gamestate == 1 or gamestate == 3  then
     
 		if key == 'p' then
 			gamestate = 2
@@ -186,6 +252,13 @@ function love.keypressed(key,unicode)
 		elseif key == 'q' then
 			gamestate = 0
 		end	
+	
+	--CREDITS KEYBOARD HANDLING
+	elseif gamestate == 4 then
+	
+		if key == 'escape' and canquit then
+			gamestate = 0
+		end	
 		
 	end
 	
@@ -197,6 +270,15 @@ function love.update(dt)
 
 	
     if gamestate == 1 then
+    
+		--Update frame for character animation
+		
+		currentMacFrame = (currentMacFrame + 18*dt) % 9
+		
+		moving = false
+		
+		--Modify worldsize (for debugging)
+		
 		if love.keyboard.isDown("u") then
 			if worldSize < 1 then
 				worldSize = worldSize + 0.001
@@ -212,38 +294,141 @@ function love.update(dt)
 		if love.keyboard.isDown(" ") and jumping == false then
 			yspeed = JUMP_POWER
 			jumping = true
+			attacking = true
 		end
 
 		jumping = true
 
-		-- Update position
+		-- Update Y position (jump)
 		yspeed = yspeed + dt*GRAVITY
-
-		macy = macy + yspeed*dt
-		if macy > screenmiddleheight+65 then
-			macy = screenmiddleheight+65
+		
+		newmacy = macy + yspeed*dt
+		
+		if newmacy > macy then
+			goingDown = true
+		else
+			goingDown = false
+		end
+		
+		macy = newmacy
+		
+		if macy > 275 and jumping and goingDown and attacking then
+			townAttack()
+			attacking = false
+		end
+		
+		if macy > screenmiddleheight+15 then
+			macy = screenmiddleheight+15
 			yspeed = 0
+			attacking = false
 			jumping = false
 		end
 		
+		
+		-- Move to the left
 		if love.keyboard.isDown("left") then
+			moving = true
 			direction = 1
-			if rotation < 1 then
-				rotation = rotation + 0.002
 			
-			else
-				rotation = -1
+			if not townCollitionLeft() then
+				if rotation < 1 then
+					rotation = rotation + 0.001
+				else
+					rotation = -1
+				end
+				
+				if skyRotation < 1 then
+					skyRotation = skyRotation + 0.0004
+				else
+					skyRotation = -1
+				end
 			end
-			
+		
+		-- Move to the right
 		elseif love.keyboard.isDown("right") then
+			moving = true
 			direction = 0
-			if rotation > -1 then
-				rotation = rotation - 0.002
-			else
-				rotation = 1
-			end	
+			
+			
+			if not townCollitionRight() then
+			
+				if rotation > -1 then
+					rotation = rotation - 0.001
+				else
+					rotation = 1
+				end	
+				
+				if skyRotation > -1 then
+					skyRotation = skyRotation - 0.0004
+				else
+					skyRotation = 1
+				end
+			end
 		end
+		
+		
+		--Check if a city has to be created
+		cityTimer = cityTimer - dt
+		
+		if cityTimer < 0 and cityCount < 5 then
+			cityTimer = 8.0
+			createCity()
+			cityCount = cityCount + 1
+		end
+		
 	end
 
+
+end
+
+
+function townCollitionRight()
+ 
+	for i,city in ipairs(cities) do
+		if (math.rad(rotation*360)+city)%math.rad(360) < math.rad(7)  and macy > 280 then
+			return true
+		end
+	end
+	return false
+	
+end
+
+function townCollitionLeft()
+ 
+	for i,city in ipairs(cities) do
+		if (math.rad(rotation*360)+city)%math.rad(360) > math.rad(352.5) and (math.rad(rotation*360)+city)%math.rad(360) < math.rad(353) and macy > 280 then
+			return true
+		end
+	end
+	return false
+	
+end
+
+
+function townAttack()
+
+	for i,city in ipairs(cities) do
+		if (math.rad(rotation*360)+city)%math.rad(360) > math.rad(355.5) or (math.rad(rotation*360)+city)%math.rad(360) < math.rad(4.5) then
+			citiesLife[i] = citiesLife[i]-1
+			if citiesLife[i] == 0 then
+				table.remove(cities,i)
+				table.remove(citiesLife,i)
+				cityCount = cityCount - 1
+			end
+		end
+	end
+	
+end
+
+function createCity()
+
+	--Create a random location for the city
+	location = math.rad(360) * math.random(0,1000)/1000
+	
+	--Add to cityList
+	table.insert(cities,location)
+
+	--Add to city life list
+	table.insert(citiesLife,3)
 
 end
